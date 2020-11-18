e5dfff679cf4f6ffa78276c3944a59de5e63cfa9
Rohit26071998 <rkkumqr2248@gmail.com>
2020-11-18 20:59:52 +0530

    amazon clone

---
+++ Amazon_Clone/src/app/Config/firebase.config.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyCA5BVR6tN4q-UmhLiDfNFqUajTVPsK65A",
+    authDomain: "my-ea2a6.firebaseapp.com",
+    databaseURL: "https://my-ea2a6.firebaseio.com",
+    projectId: "my-ea2a6",
+    storageBucket: "my-ea2a6.appspot.com",
+    messagingSenderId: "304326902413",
+    appId: "1:304326902413:web:e0bb9af9d35d7ce3fd98c4",
+    measurementId: "G-78YC6NVCDG"
+  };
